8995224d43da6d79b94384f596df84e7f34a50d8
CrazyYakov <gitarar98@yandex.ru>
2022-04-13 14:51:26 +0500

    added api for vue and update routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\JobPosition;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/getInstitutJobs', function (Request $request) {
+    if ($request['institutId']) {
+        return JobPosition::where('inst_id', $request['institutId'])->get();    
+    }
+    
+    return response(['message' => 'Bad Request', 'code' => 400], 400);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,53 @@
 <?php
 
-Route::group(['middleware'=>['auth']], function () {
-   
-    Route::get('/createDocument', 'JobDescriptionController@create');   
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/createDocument', 'JobDescriptionController@create');
+    Route::post('/createDocument/createJD', 'JobDescriptionController@createJD')->name('createDocument.createJD');
+    Route::get('/showDocuments', 'JobDescriptionController@index')->name('showDocuments');
 });
 
-Route::post('/createDocument/createJD', 'JobDescriptionController@createJD')->name('createDocument.createJD');
-Route::get('/showDocuments', 'JobDescriptionController@index')->name('showDocuments');
-
-
-Route::group(['prefix' => 'superAdmin', 'namespace'=>'superAdmin', 'middleware'=>'superAdmin'], function () {
-    
-    Route::get('/','siteController@site')->name('superAdmin.site');
+Route::group(['prefix' => 'superAdmin', 'namespace'=>'superAdmin', 'middleware'=> ['auth', 'superAdmin']], function () {
+    Route::view('/', 'superAdmin.site');
 
     Route::get('/institutions', 'InstitutionsController@index')->name('superAdmin.institutions.index');
     Route::get('/institutions/create', 'InstitutionsController@create')->name('superAdmin.institutions.create');
     Route::get('/institutions/delete', 'InstitutionsController@delete')->name('superAdmin.institutions.delete');
-    Route::get('/institutions/store', 'InstitutionsController@store')->name('superAdmin.institutions.store');
+    Route::post('/institutions/store', 'InstitutionsController@store')->name('superAdmin.institutions.store');
     
     Route::get('/users', 'UserController@index')->name('superAdmin.users.index');
     Route::get('/users/create', 'UserController@create')->name('superAdmin.users.create');
     Route::get('/users/delete', 'UserController@delete')->name('superAdmin.users.delete');
-    Route::get('/users/store/', 'UserController@store')->name('superAdmin.users.store');
+    Route::post('/users/store/', 'UserController@store')->name('superAdmin.users.store');
     
 });
 
-Route::group(['prefix' => 'moderator', 'namespace'=>'moderator','middleware' => 'moderator'], function () {
-
-    Route::get('/','siteController@site')->name('moderator.site');
+Route::group(['prefix' => 'moderator', 'namespace'=>'moderator', 'middleware' => ['auth', 'moderator']], function () {
+    Route::view('/', 'moderator.site');
 
     Route::get('/typeReqs', 'typeReqController@index')->name('moderator.typeReqs.index');
     Route::get('/typeReqs/create', 'typeReqController@create')->name('moderator.typeReqs.create');
-    Route::get('/typeReqs/store', 'typeReqController@store')->name('moderator.typeReqs.store');
+    Route::post('/typeReqs/store', 'typeReqController@store')->name('moderator.typeReqs.store');
 
     Route::get('/requirements', 'RequirementController@index')->name('moderator.requirements.index');
     Route::get('/requirements/create', 'RequirementController@create')->name('moderator.requirements.create');
-    Route::get('/requirements/store', 'RequirementController@store')->name('moderator.requirements.store');
+    Route::post('/requirements/store', 'RequirementController@store')->name('moderator.requirements.store');
 
 
     Route::get('/jobs', 'JobsController@index')->name('moderator.jobs.index');
     Route::get('/jobs/create', 'JobsController@create')->name('moderator.jobs.create');
-    Route::get('/jobs/store', 'JobsController@store')->name('moderator.jobs.store');
+    Route::post('/jobs/store', 'JobsController@store')->name('moderator.jobs.store');
 
     Route::get('/StructUnit', 'StructUnitController@index')->name('moderator.StructUnit.index');
     Route::get('/StructUnit/create', 'StructUnitController@create')->name('moderator.StructUnit.create');
-    Route::get('/StructUnit/store', 'StructUnitController@store')->name('moderator.StructUnit.store');
+    Route::post('/StructUnit/store', 'StructUnitController@store')->name('moderator.StructUnit.store');
 });
 
-Route::group(['prefix' => 'admin','namespace'=>'admin','middleware' => 'auth'], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'admin', 'middleware' => 'auth'], function () {
     Route::get('/user', 'UserController@index')->name('admin.user.index');
     Route::get('/user/create', 'UserController@create')->name('admin.user.create');
-    Route::get('/user/store', 'UserController@store')->name('admin.user.store');
+    Route::post('/user/store', 'UserController@store')->name('admin.user.store');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
